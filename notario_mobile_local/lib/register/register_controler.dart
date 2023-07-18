73b84c6b214d57600cd9bb7722bc36f812ab4b83
lukas chiaradia <lukas.chiaradia@epitech.eu>
2023-07-18 18:38:37 +0200

    Change register_controler

--- notario_mobile_local/lib/register/register_controler.dart
+++ notario_mobile_local/lib/register/register_controler.dart
@@ -1,11 +1,70 @@
-// import 'package:notario_mobile/login/connection_controler.dart';
+import 'package:notario_mobile/api/api_auth.dart';
+import 'package:http/src/response.dart';
+import 'package:notario_mobile/models/utilisateur_register.dart';
 
-// class RegisterControler {
-//   RegisterControler({
-//     required this.apiAuth
-//   });
+class RegisterControler {
+  RegisterControler({
+    required this.LastName,
+    required this.age,
+    required this.email,
+    required this.firstName,
+    required this.password,
+    required this.password_confirm,
+    this.phone,
+    required this.apiAuth,
+  });
 
-//   void changeName(String newName) {
-//     name = newName;
-//   }
-// }
+  final ApiAuth apiAuth;
+  String email;
+  String password;
+  String password_confirm;
+  String? phone;
+  String firstName;
+  String LastName;
+  String age;
+
+  void changeName(String newName) {
+    firstName = newName;
+  }
+
+  void changeLastName(String newLastName) {
+    firstName = newLastName;
+  }
+
+  void changeEmail(String newEmail) {
+    email = newEmail;
+  }
+
+  void changePassword(String newPassword) {
+    password = newPassword;
+  }
+
+  void changePasswordConfirm(String newPassword_confirm) {
+    password_confirm = newPassword_confirm;
+  }
+
+  void changeAge(String newAge) {
+    age = newAge;
+  }
+
+  void changePhone(String newPhone) {
+    age = newPhone;
+  }
+
+  Future<Response> register() async {
+    try {
+      return apiAuth.apiRegister(
+          utilisateurRegister: UtilisateurRegister(
+              LastName: LastName,
+              age: age,
+              email: email,
+              firstName: firstName,
+              password: password,
+              password_confirm: password_confirm,
+              phone: phone));
+    } catch (e) {
+      print('test');
+      throw e;
+    }
+  }
+}
