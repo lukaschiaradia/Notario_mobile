66ed3a8ccbc2ce8edd68d8f544590bd008de04f0
Tristan B <tristan.beau@epitech.eu>
2024-05-07 09:15:12 +0200

    [ADD] privacy policy

---
+++ notario_mobile_local/lib/utils/constants/privacy_policy.dart
@@ -0,0 +1,35 @@
+final String privacyPolicyText = '''
+Politique de confidentialité - Notario
+    Notario; Politique de confidentialité
+    
+  
+    Notario a développé l'application Notario en tant qu'application gratuite. Ce SERVICE est fourni par Notario gratuitement et est destiné à être utilisé tel quel.
+    
+  
+    Cette page est utilisée pour informer les visiteurs de mes politiques concernant la collecte, l'utilisation et la divulgation d'informations personnelles si quelqu'un décide d'utiliser mon Service.
+    
+  
+    L'application Notario collecte les informations personnelles suivantes lors de la création d'un compte :
+    
+      - Nom
+      - Prénom
+      - Âge
+      - Adresse e-mail
+      - Numéro de téléphone
+  
+  Ces informations sont collectées dans le but de créer et de gérer les comptes des utilisateurs, afin de leur fournir un accès personnalisé à certaines fonctionnalités de l'application.
+    
+  Ces informations personnelles ne seront en aucun cas partagées avec des tiers sans le consentement explicite de l'utilisateur, sauf dans les cas où la loi l'exige.
+    
+  Ce Service n'utilise pas de "cookies".
+    
+  Confidentialité des enfants :
+  
+    Ce Service ne s'adresse à personne de moins de 18 ans. Je ne collecte pas sciemment d'informations personnellement identifiables auprès des enfants de moins de 18 ans.
+    
+  Nous contacter :
+  
+    Si vous avez des questions ou des suggestions concernant ma politique de confidentialité, n'hésitez pas à me contacter à l'adresse suivante : notario.team@gmail.com
+  
+Ce document est non modifiable.
+''';
