dab5047bbc8c54efdf051444d16853e1e57ae216
Tristan B <tristan.beau@epitech.eu>
2024-09-04 14:56:50 +0200

    [ADD] final design of tutorial pages

--- notario_mobile_local/lib/main_page/tutorial.dart
+++ notario_mobile_local/lib/main_page/tutorial.dart
@@ -6,38 +6,215 @@ class TutorialScreen extends StatelessWidget {
   final List<Slide> slides = [
     Slide(
       title: "Tutoriel",
-      description: "Bienvenue chez Notario ! Ici vous pourrez gérer vos documents, vos rendez-vous et plus encore.",
+      widgetDescription: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Bienvenue sur l'application mobile de Notario ! Ici vous pourrez gérer vos documents, vos rendez-vous et plus encore.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
       pathImage: "images/notario.png",
       backgroundColor: Color(0xFF351EA4),
     ),
     Slide(
+      title: "Barre de navigation",
+      widgetDescription: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+             "Naviguez entre les différentes sections de l'application grâce à la barre de navigation en bas de l'écran.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      pathImage: "images/tutorial/navbar.png",
+      backgroundColor: Color(0xFF351EA4),
+    ),
+    Slide(
       title: "Documents",
-      description: "Pour consulter vos documents, accédez à la section 'Documents' depuis la NavBar. Vous pouvez visualiser ou télécharger vos fichiers importants.",
+      widgetDescription: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Pour consulter vos documents, accédez à la section 'Documents' depuis la barre de navigation. Vous pouvez visualiser ou télécharger vos fichiers importants.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
       pathImage: "images/tutorial/document.png",
       backgroundColor: Color(0xFF351EA4),
     ),
     Slide(
+      title: "Documents",
+      widgetDescription: Column(
+        children: [
+          Image.asset(
+            "images/tutorial/document2.png",
+            width: 425.0,
+            height: 425.0,
+          ),
+        ],
+      ),
+      backgroundColor: Color(0xFF351EA4),
+    ),
+    Slide(
       title: "Rendez-vous",
-      description: "Pour voir vos rendez-vous, allez dans la section 'Rendez-vous'. Vous y trouverez un calendrier avec toutes vos prochaines rencontres ou ceux passés.",
+      widgetDescription: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Pour voir vos rendez-vous, allez dans la section 'Rendez-vous'. Vous y trouverez un calendrier avec toutes vos prochaines rencontres ou ceux passés.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
       pathImage: "images/tutorial/rdv.png",
       backgroundColor: Color(0xFF351EA4),
     ),
     Slide(
       title: "FAQ",
-      description: "Des questions? Consultez notre FAQ pour des réponses rapides. Vous la trouverez dans la section 'FAQ'.",
-      pathImage: "images/tutorial/FAQ.png",
+      widgetDescription: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "images/tutorial/faq.png",
+            width: 400.0,
+            height: 400.0,
+          ),
+          Text(
+            "Des questions? Consultez notre FAQ pour des réponses rapides. Vous la trouverez dans la section 'FAQ'.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Color(0xFF351EA4),
+    ),
+    Slide(
+      title: "FAQ",
+      widgetDescription: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "images/tutorial/faq2.png",
+            width: 400.0,
+            height: 400.0,
+          ),
+          Text(
+            "Vous pouvez également filtrer les questions par catégorie pour trouver plus facilement ce que vous cherchez.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
       backgroundColor: Color(0xFF351EA4),
     ),
     Slide(
       title: "Profil",
-      description: "Pour gérer votre profil, accédez à la section 'Profil'. Vous pouvez mettre à jour vos informations personnelles et vos préférences.",
-      pathImage: "images/tutorial/profil.png",
+      widgetDescription: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "images/tutorial/profil.png",
+            width: 400.0,
+            height: 400.0,
+          ),
+          Text(
+            "Pour consulter votre profil, accédez à la section 'Profil'.",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
       backgroundColor: Color(0xFF351EA4),
     ),
     Slide(
-      title: "Profil_2",
-      description: "Depuis le menu déroulant en haut à droite du profil, vous pouvez vous lier/dissocier d'un notaire, modifier vos infos et bien plus encore !",
-      pathImage: "images/tutorial/menuderoulant.png",
+      title: "Profil",
+      widgetDescription: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "images/tutorial/profil2.png",
+            width: 400.0,
+            height: 400.0,
+          ),
+          Text(
+            "Depuis le menu déroulant vous pouvez vous lier/dissocier d'un notaire, modifier vos infos et bien plus encore !",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: Color(0xFF351EA4),
+    ),
+    Slide(
+      title: "Des retours/remarques ?",
+      widgetDescription: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            "images/tutorial/question.png",
+            width: 400.0,
+            height: 400.0,
+          ),
+          Text(
+            "N'hésitez pas à nous contacter si vous avez des retours ou des remarques sur l'application. Nous sommes à votre écoute !",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
       backgroundColor: Color(0xFF351EA4),
     ),
   ];
@@ -51,9 +228,9 @@ class TutorialScreen extends StatelessWidget {
           MaterialPageRoute(builder: (context) => Profil()),
         );
       },
-      colorDot: Colors.black,
+      colorDot: Colors.grey,
       colorActiveDot: Colors.black,
-      sizeDot: 13.0,
+      sizeDot: 8.0,
       renderNextBtn: Text('Suivant', style: TextStyle(color: Colors.white)),
       renderDoneBtn: Text('Terminer', style: TextStyle(color: Colors.white)),
       renderPrevBtn: Text('Précédent', style: TextStyle(color: Colors.white)),
@@ -61,3 +238,4 @@ class TutorialScreen extends StatelessWidget {
     );
   }
 }
+
