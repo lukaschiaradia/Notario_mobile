c7c86a8045a347cd59782d229a172d95ffbe0b3d
Tristan B <tristan.beau@epitech.eu>
2024-09-25 13:03:11 +0200

    [ADD] Nous contacter + Votre avis compte

--- notario_mobile_local/lib/main_page/settingsPage.dart
+++ notario_mobile_local/lib/main_page/settingsPage.dart
@@ -14,86 +14,138 @@ class SettingsPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-        'Paramètres',
-        style: TextStyle(color: Colors.white),
-      ),
+          'Paramètres',
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Color(0xFF351EA4),
       ),
       backgroundColor: Color(0xFF1A1B25),
       body: ListView(
         children: [
-          ListTile(
-      leading: Icon(Icons.help_outline, color: Colors.white),
-      title: Text(
-        'Tutoriel',
-        style: TextStyle(color: Colors.white),
+          // Bloc Général
+          _buildSectionHeader('Général'),
+          _buildGeneralSettings(context),
+
+          // Bloc Compte
+          _buildSectionHeader('Compte'),
+          _buildAccountSettings(context),
+
+          // Bloc Support
+          _buildSectionHeader('Support'),
+          _buildSupportSettings(context),
+
+          // Bloc Ressources
+          _buildSectionHeader('Ressources'),
+          _buildResourceSettings(context),
+
+          // Ajout d'un padding en bas
+          SizedBox(height: 20),
+        ],
       ),
-      onTap: () {
-        _showTutorialConfirmationDialog(context);
-      },
-    ),
-    ListTile(
-      leading: Icon(Icons.logout, color: Colors.white),
-      title: Text(
-        'Déconnexion',
-        style: TextStyle(color: Colors.white),
+    );
+  }
+
+  // Widget pour créer un en-tête de section
+  Widget _buildSectionHeader(String title) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        title,
+        style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
       ),
-      onTap: () {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => ConnexionPage()),
-        );
-      },
-    ),
-    ListTile(
-      leading: Icon(Icons.delete, color: Colors.white),
-      title: Text(
-        'Supprimer mon compte',
-        style: TextStyle(color: Colors.white),
-      ),
-      onTap: () {
-        _showDeleteDialog(context);
-      },
-    ),
-    ListTile(
-      leading: Icon(Icons.privacy_tip, color: Colors.white),
-      title: Text(
-        'Politique de confidentialité',
-        style: TextStyle(color: Colors.white),
-      ),
-      onTap: () {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text(
-                'Politique de confidentialité',
-                style: TextStyle(color: Colors.white),
-              ),
-              content: SingleChildScrollView(
-                child: Text(
-                  privacyPolicyText,
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              backgroundColor: Color(0xFF351EA4),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text(
-                    'Fermer',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
+    );
+  }
+
+  // Fonction pour construire le bloc Général
+  Widget _buildGeneralSettings(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(Icons.help_outline, color: Colors.white),
+          title: Text(
+            'Tutoriel',
+            style: TextStyle(color: Colors.white),
+          ),
+          onTap: () {
+            _showTutorialConfirmationDialog(context);
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.logout, color: Colors.white),
+          title: Text(
+            'Déconnexion',
+            style: TextStyle(color: Colors.white),
+          ),
+          onTap: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => ConnexionPage()),
             );
           },
-        );
-      },
-    ),
-    ListTile(
+        ),
+      ],
+    );
+  }
+
+  // Fonction pour construire le bloc Compte
+  Widget _buildAccountSettings(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(Icons.delete, color: Colors.white),
+          title: Text(
+            'Supprimer mon compte',
+            style: TextStyle(color: Colors.white),
+          ),
+          onTap: () {
+            _showDeleteDialog(context);
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.privacy_tip, color: Colors.white),
+          title: Text(
+            'Politique de confidentialité',
+            style: TextStyle(color: Colors.white),
+          ),
+          onTap: () {
+            _showPrivacyPolicyDialog(context);
+          },
+        ),
+      ],
+    );
+  }
+
+  // Fonction pour construire le bloc Support
+  Widget _buildSupportSettings(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(Icons.contact_mail, color: Colors.white),
+          title: Text(
+            'Nous contacter',
+            style: TextStyle(color: Colors.white),
+          ),
+          onTap: () {
+            _showContactDialog(context);
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.feedback, color: Colors.white),
+          title: Text(
+            'Votre avis compte',
+            style: TextStyle(color: Colors.white),
+          ),
+          onTap: () {
+            _showFeedbackDialog(context);
+          },
+        ),
+      ],
+    );
+  }
+
+  // Fonction pour construire le bloc Ressources
+  Widget _buildResourceSettings(BuildContext context) {
+    return ListTile(
       leading: Icon(Icons.web, color: Colors.white),
       title: Text(
         'Visiter notre site web',
@@ -102,9 +154,105 @@ class SettingsPage extends StatelessWidget {
       onTap: () {
         _launchURL('http://20.111.31.171:3000');
       },
-    ),
-        ],
-      ),
+    );
+  }
+
+  // Fonction pour montrer le dialogue de confidentialité
+  void _showPrivacyPolicyDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Politique de confidentialité',
+            style: TextStyle(color: Colors.white),
+          ),
+          content: SingleChildScrollView(
+            child: Text(
+              privacyPolicyText,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          backgroundColor: Color(0xFF351EA4),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Fermer',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+  void _showContactDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Nous contacter', style: TextStyle(color: Colors.white)),
+          content: Text('Voulez-vous ouvrir votre gestionnaire de mail ?',
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: Color(0xFF351EA4),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Non', style: TextStyle(color: Colors.white)),
+            ),
+            TextButton(
+              onPressed: () {
+                _launchEmail();
+                Navigator.of(context).pop();
+              },
+              child: Text('Oui', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _launchEmail() async {
+    final Uri emailLaunchUri = Uri(
+      scheme: 'mailto',
+      path: 'notario.team@gmail.com',
+    );
+
+    await launch(emailLaunchUri.toString());
+  }
+
+  void _showFeedbackDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Votre avis compte', style: TextStyle(color: Colors.white)),
+          content: Text('Voulez-vous ouvrir le questionnaire de satisfaction ?',
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: Color(0xFF351EA4),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Non', style: TextStyle(color: Colors.white)),
+            ),
+            TextButton(
+              onPressed: () {
+                _launchURL2('http://youtube.com');
+                Navigator.of(context).pop();
+              },
+              child: Text('Oui', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
+      },
     );
   }
 
@@ -202,6 +350,14 @@ class SettingsPage extends StatelessWidget {
     final Uri uri = Uri.parse(url);
     if (await canLaunch(uri.toString())) {
       await launch(uri.toString());
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  void _launchURL2(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
     } else {
       throw 'Could not launch $url';
     }
