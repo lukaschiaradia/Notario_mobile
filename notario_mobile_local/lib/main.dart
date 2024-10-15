98101c92ac3f22b4c835b8174fd5581e204a405f
Tristan B <tristan.beau@epitech.eu>
2024-10-15 18:11:50 +0200

    [ADD] New Visualization and Download for .docx

--- notario_mobile_local/lib/main.dart
+++ notario_mobile_local/lib/main.dart
@@ -1,44 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:notario_mobile/main_page/profil_page.dart';
 import 'welcome_page.dart';
+import 'login/connection_controler.dart';
+import 'api/api_auth.dart';
+import 'main_page/faq_page.dart';
 
 const blue_color = Color(0xFF6949FF);
 
-void main() {
+void main() async {
   AwesomeNotifications().initialize(
-      null,
-      [
-        NotificationChannel(
-            channelKey: 'alerts',
-            channelName: 'Alerts',
-            channelDescription: 'Notification tests as alerts')
-      ],
-      debug: true);
+    null,
+    [
+      NotificationChannel(
+        channelKey: 'alerts',
+        channelName: 'Alerts',
+        channelDescription: 'Notification tests as alerts',
+      ),
+    ],
+    debug: true,
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
-  runApp(const MyApp());
+  ApiAuth apiAuth = ApiAuth();
+  ConnectionControler connectionControler = ConnectionControler(apiAuth: apiAuth);
+  
+  await connectionControler.loadCredentials();
+
+  runApp(MyApp(connectionControler: connectionControler));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  final ConnectionControler connectionControler;
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
+  const MyApp({super.key, required this.connectionControler});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Notario',
-      //debugShowCheckedModeBanner: false,
-      home: WelcomePage(),
-
+      debugShowCheckedModeBanner: false,
+      home: FutureBuilder(
+        future: connectionControler.loadCredentials(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return Center(child: Text("Erreur de chargement : ${snapshot.error}"));
+          }
+          if (connectionControler.stayLoggedIn) {
+            return Profil();
+          }
+          return WelcomePage();
+        },
+      ),
     );
   }
 }
