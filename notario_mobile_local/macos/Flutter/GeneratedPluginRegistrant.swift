6781ddc6caf379accda769c7cca2f56f5ffdb37a
Tristan B <tristan.beau@epitech.eu>
2024-10-15 17:55:18 +0200

    [ADD] New Visualization and Download for .docx

--- notario_mobile_local/macos/Flutter/GeneratedPluginRegistrant.swift
+++ notario_mobile_local/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import awesome_notifications
 import device_info_plus
+import open_file_mac
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite
@@ -16,6 +17,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
