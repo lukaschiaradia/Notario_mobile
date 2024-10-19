52a4257e316b40b945e295ba3329bc58fc22f19b
tristan-guepeed <tristan.beau@epitech.eu>
2024-10-19 20:00:49 +0200

    [ADD] new release

--- notario_mobile_local/macos/Flutter/GeneratedPluginRegistrant.swift
+++ notario_mobile_local/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import device_info_plus
 import open_file_mac
 import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import syncfusion_pdfviewer_macos
 import url_launcher_macos
 
